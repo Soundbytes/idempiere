a7756d48afebc9c58b4e234cfac97133579c8efd
Andreas <sumerauer@kanzlei-wmv.de>
2021-06-12 12:45:34 +0200

    WQuickform, IQuickform refactored

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/apps/form/IQuickForm.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/apps/form/IQuickForm.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (C) 2016 Logilite Technologies LLP								  *
+ * Copyright (C) Contributors                                                 *
  * This program is free software; you can redistribute it and/or modify it    *
  * under the terms version 2 of the GNU General Public License as published   *
  * by the Free Software Foundation. This program is distributed in the hope   *
@@ -9,58 +9,24 @@
  * You should have received a copy of the GNU General Public License along    *
  * with this program; if not, write to the Free Software Foundation, Inc.,    *
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * Contributors:                                                              *
+ *   Andreas Sumerauer                                                        *
  *****************************************************************************/
 
 package org.adempiere.webui.apps.form;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.adempiere.util.Callback;
-import org.adempiere.webui.adwindow.AbstractADWindowContent;
-import org.adempiere.webui.adwindow.QuickGridView;
-import org.adempiere.webui.component.Borderlayout;
-import org.adempiere.webui.component.Button;
-import org.adempiere.webui.component.ConfirmPanel;
-import org.adempiere.webui.component.Panel;
-import org.adempiere.webui.component.Window;
-import org.adempiere.webui.component.ZkCssHelper;
-import org.adempiere.webui.session.SessionManager;
-import org.adempiere.webui.util.ZKUpdateUtil;
-import org.adempiere.webui.window.CustomizeGridViewDialog;
-import org.adempiere.webui.window.FDialog;
-import org.compiere.model.DataStatusEvent;
 import org.compiere.model.DataStatusListener;
-import org.compiere.model.GridField;
-import org.compiere.model.GridTab;
-import org.compiere.model.MRole;
-import org.compiere.util.Env;
-import org.compiere.util.Msg;
-import org.compiere.util.Trx;
-import org.zkforge.keylistener.Keylistener;
-import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.EventListener;
-import org.zkoss.zk.ui.event.Events;
-import org.zkoss.zul.Column;
-import org.zkoss.zul.Columns;
 
 /**
- * Quick entry form
+ * Quick entry form Interface based on WQuickForm by Logilite
  * 
- * @author Logilite Technologies
- * @since Nov 03, 2017
+ * @author Andreas Sumerauer
+ * 
  */
 public interface IQuickForm extends EventListener <Event>, DataStatusListener
 {
-//	public Trx trx = null;
-
-//	public IQuickForm(AbstractADWindowContent winContent, boolean m_onlyCurrentRows, int m_onlyCurrentDays);
-
 	public void onCancel();
 
 	public void onUnSort();
